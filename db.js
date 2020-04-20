46d45796a5c8587d40bd115f0142d783f575547e
LiYiuShing <18022213d@connect.polyu.hk>
2020-04-20 12:07:20 +0800

    Init

---
+++ db.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+var connect = () => {
+    return mongoose.connect(
+        process.env.DB_CONNECT, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        .then(() => {
+            console.log('MONGODB CONNECTED')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+};
+
+module.exports = connect;
